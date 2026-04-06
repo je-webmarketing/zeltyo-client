9ee883549fd6637b74cd254197313a4e3a45ff2e
JE Webmarketing <contact@je-webmarketing.com>
2026-04-06 08:23:01 +0200

    fix reset password routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./AppLive.jsx";
-import { app } from "./firebase";
+import App from "./ClientApp.jsx";
+import ResetPassword from "./ResetPassword.jsx";
+import "./index.css";
+
+const pathname = window.location.pathname;
+
+const RootComponent = pathname.startsWith("/reset-password")
+  ? ResetPassword
+  : App;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RootComponent />
   </React.StrictMode>
 );
